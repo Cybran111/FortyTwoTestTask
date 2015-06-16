8464cbb9dc2a201029aa88e509dc7928495c4a25
Nikita Lyubchich <cybran111@gmail.com>
2015-06-16 19:56:05 +0300

    ticket:1 Fixing admin panel, added new fields to Profile, provided fixture

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model())
+    birth_date = models.DateField()
+    bio = models.TextField()
+    contacts = models.TextField()
