0c8f351bc04b32d46f74cfe171e9de3ec09e2336
Nikita Lyubchich <cybran111@gmail.com>
2015-06-17 09:56:08 +0300

    ticket:1 Added some tests

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
@@ -9,4 +10,13 @@ class SomeTests(TestCase):
 
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_homepage_correct_template(self):
+        """Is view uses correct template?"""
+        response = self.client.get('/')
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_homepage_context_correct(self):
+        """Is view provides correct context?"""
+        response = self.client.get('/')
+        self.assertEqual(User.objects.get(pk=1), response.context["person"])
