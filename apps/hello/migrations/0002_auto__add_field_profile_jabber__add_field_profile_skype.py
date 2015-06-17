727abec45b29d1e3dd38e524e0c0b2dc0442452d
Nikita Lyubchich <cybran111@gmail.com>
2015-06-17 12:27:15 +0300

    ticket:1 Fixing migration

--- apps/hello/migrations/0002_auto__add_field_profile_jabber__add_field_profile_skype.py
+++ apps/hello/migrations/0002_auto__add_field_profile_jabber__add_field_profile_skype.py
@@ -1,1 +1,10 @@
 "Dumb migration"
+from south.v2 import SchemaMigration
+
+
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        pass
+
+    def backwards(self, orm):
+        pass
