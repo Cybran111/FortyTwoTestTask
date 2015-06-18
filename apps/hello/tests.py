bda676a4be6b05ce934efe2e7fb5a20e16cc4b40
Nikita Lyubchich <cybran111@gmail.com>
2015-06-18 14:08:34 +0300

    ticket:3 Created tests for Requests middleware and view

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -4,10 +4,9 @@ from django.test import TestCase
 # Create your tests here.
 
 
-class SomeTests(TestCase):
+class HomePageTests(TestCase):
     def test_homepage_exists(self):
         """Is homepage accessable?"""
-
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
@@ -20,3 +19,42 @@ class SomeTests(TestCase):
         """Is view provides correct context?"""
         response = self.client.get('/')
         self.assertEqual(User.objects.get(pk=1), response.context["person"])
+
+class RequestsPageTests(TestCase):
+    def test_page_exists(self):
+        """Is Requests page accessable?"""
+        response = self.client.get('/requests/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_correct_template(self):
+        """Is view uses correct template?"""
+        response = self.client.get('/requests/')
+        self.assertTemplateUsed(response, 'requests.html')
+
+    def test_homepage_context_correct(self):
+        """Is view provides correct context?"""
+        response = self.client.get('/requests/')
+        self.assertEqual(Request.objects.all()[:10], response.context["requests"])
+
+class RequestMiddlewareTest(TestCase):
+    def test_middleware_catches_good_get(self):
+        """Is middleware saves good GET request?"""
+        self.client.get('/')  # homepage
+        self.assertEqual(1, Request.objects.count())
+
+    def test_middleware_catches_bad_get(self):
+        """Is middleware saves bad GET request?"""
+        self.client.get('/somedumblink/')
+        self.assertEqual(1, Request.objects.count())
+
+    def test_middleware_catches_good_post(self):
+        """Is middleware saves good POST request?"""
+        self.client.post('/')  # homepage
+        self.assertEqual(1, Request.objects.count())
+
+    def test_middleware_catches_bad_post(self):
+        """Is middleware saves bad POST request?"""
+        self.client.post('/somedumblink/')
+        self.assertEqual(1, Request.objects.count())
+
+
