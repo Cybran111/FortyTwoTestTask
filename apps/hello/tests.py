44f543590a21e8d5930f66b748d9c87ba3bf45c1
Nikita Lyubchich <cybran111@gmail.com>
2015-06-19 08:32:52 +0300

    ticket:3 Added tests for AJAX Request view, fixed requests template

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core import serializers
 from django.test import TestCase
 
 # Create your tests here.
@@ -72,3 +73,32 @@ class RequestMiddlewareTest(TestCase):
         """Is middleware saves bad POST request?"""
         self.client.post('/somedumblink/')
         self.assertEqual(1, Request.objects.count())
+
+
+class RequestsListTest(TestCase):
+    def setUp(self):
+        self.LAST_ITEM_DELTA = 4  # No matters how far from the end
+        self.not_last_item = Request.objects.count() - self.LAST_ITEM_DELTA
+
+    def test_view_returns_right_value_without_providing_last_item_id(self):
+        """If we do not providing id of last item, view should return last 10 items"""
+        response = self.client.get('/requests/list/', HTTP_X_REQUESTED_WITH='XMLHttpRequest')  # AJAX request
+        requests = serializers.serialize('json', list(Request.objects.all()[:10]))
+
+        self.assertEqual(requests, response.body)
+
+    def test_view_returns_right_value_with_providing_last_item_id(self):
+        """If we providing id of last item, view should return JSON accordingly to this value"""
+        response = self.client.get('/requests/list/',
+                                   {"last": self.not_last_item},
+                                   HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        requests = serializers.serialize('json', list(Request.objects.all()[:self.LAST_ITEM_DELTA]))
+        self.assertEqual(requests, response.body)
+
+    def test_view_process_last_item_correctly(self):
+        """In case when there are no new requests, view should return empty JSON"""
+        response = self.client.get('/requests/list/',
+                               {"last": Request.objects.count()},
+                               HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        requests = serializers.serialize('json', list())
+        self.assertEqual(requests, response.body)
