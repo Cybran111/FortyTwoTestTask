a5abf41d5e1e588eddf5b971726f47a7bde8198a
Nikita Lyubchich <cybran111@gmail.com>
2015-06-19 14:02:33 +0300

    ticket:1 Updated admin panel, rewrited template, added Bootstrap and logging, refactored tests

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from apps.hello.models import Profile, Request
 # Register your models here.
 
-admin.site.register((Profile, Request))
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'firstname', 'lastname', 'birth_date', 'jabber')
+
+    def firstname(self, instance):
+        return instance.user.first_name
+
+    def lastname(self, instance):
+        return instance.user.last_name
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Request)
