a7827a7f382a06ae3a693e552f5018584a6b9ed5
Nikita Lyubchich <cybran111@gmail.com>
2015-06-22 08:07:56 +0300

    ticket:3 middleware ignores requests periodic polling

--- apps/hello/middleware.py
+++ apps/hello/middleware.py
@@ -3,4 +3,6 @@ from apps.hello.models import Request
 
 class RequestsMiddleware:
     def process_request(self, request):
-        Request.objects.create(method=request.method, path=request.path)
+        # Can be transformed into list of filters
+        if (request.path != resolve('requests_list')):
+            Request.objects.create(method=request.method, path=request.path)
