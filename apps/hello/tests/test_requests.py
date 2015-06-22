c07ba8feb4e4b14d3733de4f50577bf2c3425d39
Nikita Lyubchich <cybran111@gmail.com>
2015-06-22 21:15:05 +0300

    ticket:3 Rewrited tests

--- apps/hello/tests/test_requests.py
+++ apps/hello/tests/test_requests.py
@@ -55,35 +55,26 @@ class RequestsListTest(TestCase):
 
     def setUp(self):
         self.LAST_ITEM_DELTA = 4  # No matters how far from the end
+        self.REQUESTS_COUNT = Request.objects.count()
 
     def test_view_correct_list_without_providing_last_item_id(self):
         """If we do not providing id of last item,
-        view should return last 10 items"""
-        response = self.client.get('/requests/list/')  # AJAX request
-        requests = serializers.serialize(
-            'json',
-            list(Request.objects.order_by("created_at")[:10])
-        )
-        self.assertEqual(requests, response.content)
+        view should return HTTP 400 Bad Request"""
+        response = self.client.get('/requests/list/')
+        self.assertEqual(400, response.status_code)
 
     def test_view_correct_list_with_providing_last_item_id(self):
         """If we providing id of last item,
         view should return JSON accordingly to this value"""
+
         response = self.client.get(
             '/requests/list/',
-            {"last_id": Request.objects.count() - self.LAST_ITEM_DELTA})
+            {"last_id": self.REQUESTS_COUNT - self.LAST_ITEM_DELTA}
+        )
+
         requests = serializers.serialize(
             'json',
-            list(Request.objects.all()[:self.LAST_ITEM_DELTA])
+            list(Request.objects.order_by("created_at")[self.REQUESTS_COUNT-self.LAST_ITEM_DELTA:])
         )
-        self.assertEqual(requests, response.content)
 
-    def test_view_process_last_item_correctly(self):
-        """In case when there are no new requests,
-        view should return only this request in list"""
-        self.client.get('/admin/')
-        response = self.client.get('/requests/list/',
-                                   {"last_id": Request.objects.count()-1})
-        requests = serializers.serialize('json',
-                                         list(Request.objects.order_by("created_at")[0:1]))
         self.assertEqual(requests, response.content)
