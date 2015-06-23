19e2234633122acf566f8f5399631ebf06dacf07
Nikita Lyubchich <cybran111@gmail.com>
2015-06-23 18:05:24 +0300

    ticket:5 Fixing test

--- apps/hello/tests/test_profile_model.py
+++ apps/hello/tests/test_profile_model.py
@@ -1,20 +1,19 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
-User = get_user_model()
+from apps.hello.models import Profile
 
 
 class ProfileModelTests(TestCase):
     def test_model_can_return_dict_repr(self):
-        user = User.objects.get(pk=1)
+        profile = Profile.objects.get(pk=1)
         profile_dict = {
-            "first_name": user.first_name,
-            "last_name": user.first_name,
-            "birth_date": user.profile.birth_date,
-            "photo": user.profile.photo,
-            "email": user.profile.email,
-            "jabber": user.profile.jabber,
-            "skype": user.profile.skype,
-            "contacts": user.profile.contacts,
-            "bio": user.profile.bio,
+            "first_name": profile.user.first_name,
+            "last_name": profile.user.first_name,
+            "birth_date": profile.birth_date,
+            "photo": profile.photo,
+            "email": profile.user.email,
+            "jabber": profile.jabber,
+            "skype": profile.skype,
+            "contacts": profile.contacts,
+            "bio": profile.bio,
         }
-        self.assertDictEqual(user.to_dict(), profile_dict)
+        self.assertDictEqual(profile.to_dict(), profile_dict)
