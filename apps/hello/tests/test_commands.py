25b0ae1ab63a162d4b8f0f49e3dcf11dc5732c1c
Nikita Lyubchich <cybran111@gmail.com>
2015-06-26 17:53:23 +0300

    ticket:8 Added test for dublicate stdout to stderr for getmodels command Fixed test for correct result

--- apps/hello/tests/test_commands.py
+++ apps/hello/tests/test_commands.py
@@ -1,20 +1,47 @@
-import json
-from django.core import serializers
+from StringIO import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 from django.db import models
+import sys
+
 
 class CommandGetModelsTests(TestCase):
     def test_getmodels_returns_all_models_data(self):
-        """getmodels should return JSON with all models and objects count in them"""
+        """getmodels should return all models
+        line by line and objects count in them"""
+
         model_list = models.get_models(include_auto_created=True)
-        expected_result = json.dumps({
-            u"%s-%s" % (model._meta.app_label.lower(), model._meta.object_name.lower()): model.objects.count()
-            for model in model_list
-            })
-        actual_result = call_command('getmodels')
+
+        expected_list = ['%s-%s - %d' %
+                         (
+                             model._meta.app_label.lower(),
+                             model._meta.object_name.lower(),
+                             model.objects.count()
+                         )
+                         for model in model_list]
+
+        expected_result = '\n'.join(expected_list)+'\n'
+        out, sys.stdout = sys.stdout, StringIO()
+        call_command('getmodels')
+
+        sys.stdout.seek(0)
+
+        actual_result = unicode(sys.stdout.read())
+        sys.stdout = out
 
         self.assertEqual(expected_result, actual_result)
 
     def test_getmodels_dublicates_stdout_to_stderr(self):
-        pass
+        """Command should dublicate stdout to stderr"""
+        out, sys.stdout = sys.stdout, StringIO()
+        err, sys.stderr = sys.stderr, StringIO()
+
+        call_command('getmodels')
+
+        sys.stdout.seek(0)
+        sys.stderr.seek(0)
+
+        self.assertEqual(sys.stdout.read(), sys.stderr.read())
+
+        sys.stdout = out
+        sys.stderr = err
