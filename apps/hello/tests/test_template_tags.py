31d02846ac753266d428e34b87e33b0824104581
Nikita Lyubchich <cybran111@gmail.com>
2015-06-26 14:24:49 +0300

    ticket:8 Added editlink template tag Some fixes in tests

--- apps/hello/tests/test_template_tags.py
+++ apps/hello/tests/test_template_tags.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from apps.hello.models import Request
+from apps.hello.templatetags.admin_tags import editlink, BadModelException
 
 
 class EditLinkTagTests(TestCase):
@@ -10,15 +11,17 @@ class EditLinkTagTests(TestCase):
     def test_tag_returns_user_admin_link(self):
         """editlink should return correct link for edit user in admin panel"""
         user = User.objects.get(id=1)
+        expected_url = reverse("admin:auth_user_change", args=[user.id])
+
         actual_url = editlink(user)
-        expected_url = reverse("admin:user_change", args=[user.id])
         self.assertEqual(actual_url, expected_url)
 
     def test_tag_returns_request_admin_link(self):
-        """editlink should return correct link for edit request in admin panel"""
+        """editlink should return correct link
+        for edit request in admin panel"""
         req = Request.objects.get(id=1)
+        expected_url = reverse("admin:hello_request_change", args=[req.id])
         actual_url = editlink(req)
-        expected_url = reverse("admin:request_change", args=[req.id])
         self.assertEqual(actual_url, expected_url)
 
     def test_tag_throws_error_for_wrong_object(self):
