f9bdc3a0c287abf480b3fdaf1148eddb4b020809
Nikita Lyubchich <cybran111@gmail.com>
2015-06-29 08:41:21 +0300

    ticket:8 editpage view provides user model fixed test and template

--- apps/hello/tests/test_editpage.py
+++ apps/hello/tests/test_editpage.py
@@ -38,7 +38,8 @@ class EditPersonPageTests(TestCase):
 
     def test_editpage_context_correct(self):
         """Is view provides correct context?"""
-        self.assertEqual(User.objects.get(pk=1), self.response.context["person"])
+        self.assertEqual(User.objects.get(pk=1),
+                         self.response.context["person"])
         self.assertIsInstance(self.response.context["editform"],
                               EditProfileForm)
 
